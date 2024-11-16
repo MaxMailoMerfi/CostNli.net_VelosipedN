a4f9fabfba548f07a4260a4726ce38aa171501c8
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-11-17 00:01:30 +0200

    Ставка

--- CostNl_2.net/CostNl_2.1.net/Program.cs
+++ CostNl_2.net/CostNl_2.1.net/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new Random();
+
+            Console.WriteLine("Вас приветсвует йобаное казино");
+            for (;;)
+            {
+                Console.WriteLine("\nСделайте ставку то 5$ до 100$, з шагом 5$");
+                double bid = double.Parse(Console.ReadLine());
+                if ((bid % 5 == 0) && (bid > 0) )
+                {
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("\nНекорректный ввод.");
+                }
+
+                
+            }
         }
     }
 }
