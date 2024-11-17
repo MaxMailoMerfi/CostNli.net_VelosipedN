e4c8ba534f6e82f31d59ccd30e865e64cae3b831
Максим Олександрович Зенченко <zenchmax@gmail.com>
2024-11-17 21:10:52 +0200

    Без анімації

--- CostNl_2.net/CostNl_2.1.net/Program.cs
+++ CostNl_2.net/CostNl_2.1.net/Program.cs
@@ -5,9 +5,9 @@
         static void Main(string[] args)
         {
             Random random = new Random();
-            int bid, one, tuo, thre;
-            int hack = 42, bid_hack = 5; // читы
-            string kazino = "$Казино$"; //Название йобаного казино
+            int bid; int one, tuo, thre;
+            int hack = 42; int bid_hack = -1; // читы (код, сколько бабок
+            string kazino = "$Казино$"; //Название Которое будет сверху йобаного казино
 
             Console.WriteLine("Вас приветсвует йобаное казино");
 
@@ -26,15 +26,16 @@
                     {
                         Console.Clear();
                         Console.WriteLine(kazino);
+
                         Console.WriteLine("\nНекорректный ввод");
                     }
 
                 }
                 if (bid == hack) //Хак
                 {
-                    bid = bid_hack;
                     Console.WriteLine(kazino);
-                    Console.WriteLine($"Ля ты криса но ставка будет = {bid}");
+                    bid = bid_hack;
+                    Console.WriteLine($"Ля ты криса но ставка будет = {bid_hack}");
                     one = 7;
                     tuo = 7;
                     thre = 7;
@@ -42,16 +43,56 @@
                 else //Рандом
                 {
                     Console.WriteLine(kazino);
+
                     one = random.Next(1, 10);
                     tuo = random.Next(1, 10);
                     thre = random.Next(1, 10);
                 }
                 Console.WriteLine($"\n{one}{tuo}{thre}");
-                
+                double Win;
                 //+++
-                if ((one == tuo) || (one == thre))
-                { 
-                    
+                if ((one == tuo) && (one == thre))
+                {
+                    if (one == 7)
+                    {
+                        Win = bid_hack * 150 * 1.5;
+                        Console.WriteLine($"\nДжекпот будет составлять = {Win}");
+                    }
+                    else
+                    {
+                        Win = bid * (one * 10) * 1.5;
+                        Console.WriteLine($"Ля ты криса но ставка будет = {Win}");
+                    }
+
+                }
+                else
+                {
+                    if ((one == tuo) || (one == thre))
+                    {
+                        if (one == 7)
+                        {
+                            Win = bid_hack * 15 * 1.25;
+                            Console.WriteLine($"\nДжекпот будет составлять = {Win}");
+                        }
+                        else
+                        {
+                            Win = bid * one * 1.25;
+                            Console.WriteLine($"Ля ты криса но ставка будет = {Win}");
+                        }
+                    }
+                    if (tuo == thre)
+                    {
+                        if (tuo == 7)
+                        {
+                            Win = bid * 15 * 1.25;
+                            Console.WriteLine($"\nДжекпот будет составлять = {Win}");
+                        }
+                        else
+                        {
+                            Win = bid * tuo * 1.25;
+                            Console.WriteLine($"Ля ты криса но ставка будет = {Win}");
+                        }
+                    }
                 }
             }
         }
